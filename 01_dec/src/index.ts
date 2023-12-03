091a36adfc13929bd130c2b8a443212fb231cd40
SarahZwahlen <sarah.zwahlen@wanadoo.fr>
2023-12-03 15:47:50 +0100

    set up 1 december problem

---
+++ 01_dec/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 
+    Stocker les différentes valeurs dans un tableau
+    Créer les paires de digit : 
+    Lire la ligne
+        Lire chaque caractère
+        Déterminer si chaque caractère est un nombre ou une lettre
+        ne retourner que si c'est un nombre --> Je dois donc stocker à la fois la ligne après parsing, et l'ensemble des lignes parsées
+        
+
+    Additionner toutes les valeurs pour obtenir la valeur finale
+*/
